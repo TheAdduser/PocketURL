ae6cf7012da0273eea82a7716ff53f56a40d0b8f
avgmathenjoyer <avgmathenjoyer@gmail.com>
2026-01-10 15:58:15 +0100

    a

--- frontend/app/constants.ts
+++ frontend/app/constants.ts
@@ -1,2 +1,2 @@
 export const API_URL = "https://pocket-url-backend-dev.onrender.com";
-export const FRONTEND_URL = "https://pocket-url-frontend-dev-2.onrender.com";
+export const FRONTEND_URL = "https://pocket-url-frontend-dev.onrender.com";
