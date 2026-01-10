5e055b888405caf2aa045d8df6de107cc88e82ff
michalgs <michalgsmoragiewicz@gmail.com>
2026-01-10 15:03:48 +0100

    Refactoring

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
+import os
 from .decorators import singleton
-
 
 @singleton
 class Config:
@@ -11,6 +11,9 @@ class Config:
         # backend
         self.urls_database_path = "./database/urls.db"
         self.metrics_database_path = "./database/metrics.db"
+        self.connection_string = os.getenv("DB_CONNECTION_STRING")
+        self.port = int(os.getenv("PORT", 8000))
+        self.ELEMENTS_PER_PAGE = 10
 
         # frontend
 
